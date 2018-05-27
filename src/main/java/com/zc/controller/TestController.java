5b8240fe085956e214dfb659ba78b831a1b7eff7
zhousb <15067137047@163.com>
2018-05-27 23:59:13 +0800

    test002

--- src/main/java/com/zc/controller/TestController.java
+++ src/main/java/com/zc/controller/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("test")
 public class TestController {
 	
-	@RequestMapping("testController.do")
+	@RequestMapping("testCon.do")
 	public String test(){
 		return "test";
 	}
